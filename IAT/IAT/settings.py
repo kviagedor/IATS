d42be9b5670d2286ec7689b29a8d6fba764d9e41
Keneth Viagedor <keneth.viagedor@gmail.com>
2024-05-23 08:34:42 +0800

    update settings

--- IAT/IAT/settings.py
+++ IAT/IAT/settings.py
@@ -121,6 +121,10 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
 
 # Default primary key field type
